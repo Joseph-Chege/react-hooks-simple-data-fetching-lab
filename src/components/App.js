2a1b95775dd3ce251e59c78c9dc4056aed587ee4
Joseph-Chege <mjchege23@gmail.com>
2024-06-01 13:16:25 +0300

    Used the useEffect method to query data from an API

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useState, useEffect} from 'react'
+
+function App() {
+    const [isDog, setisDog] = useState('')
+    const [isLoaded, setisLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setisDog(data.message);
+            setisLoaded(true)
+        });
+    }, []);
+
+    if (!isLoaded) return <p>Loading... </p>
+    return (
+       
+    <div>
+        <img
+            key={isDog} 
+            src={isDog} 
+            alt= "A Random Dog"
+        />
+    </div>
+  )
+}
+
+export default App;
